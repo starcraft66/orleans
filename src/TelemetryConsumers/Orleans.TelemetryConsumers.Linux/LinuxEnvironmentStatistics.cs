c61e1d1148fb51cc50047043937a48d9e6722b2e
Tristan Gosselin-Hane <starcraft66@gmail.com>
2021-10-14 15:06:28 -0400

    Initial cgroups V1 support for LinuxEnvironmentStatistics

--- src/TelemetryConsumers/Orleans.TelemetryConsumers.Linux/LinuxEnvironmentStatistics.cs
+++ src/TelemetryConsumers/Orleans.TelemetryConsumers.Linux/LinuxEnvironmentStatistics.cs
@@ -27,6 +27,9 @@ namespace Orleans.Statistics
         public long? AvailableMemory { get; private set; }
 
         /// <inheritdoc />
+        public bool InCGroup { get; private set; }
+
+        /// <inheritdoc />
         private long MemoryUsage => GC.GetTotalMemory(false);
 
         private readonly TimeSpan MONITOR_PERIOD = TimeSpan.FromSeconds(5);
@@ -36,11 +39,23 @@ namespace Orleans.Statistics
 
         private const string MEMINFO_FILEPATH = "/proc/meminfo";
         private const string CPUSTAT_FILEPATH = "/proc/stat";
+        private const string CGROUP_V1_FILEPATH = "/sys/fs/cgroup";
+        private const string CGROUP_V1_USED_MEM_FILEPATH = "/sys/fs/cgroup/memory/memory.usage_in_bytes";
+        private const string CGROUP_V1_TOTAL_MEM_FILEPATH = "/sys/fs/cgroup/memory/memory.limit_in_bytes";
+        private const string CGROUP_V1_CFS_QUOTA_FILEPATH = "/sys/fs/cgroup/cpuacct/cpu.cfs_quota_us";
+        private const string CGROUP_V1_CFS_PERIOD_FILEPATH = "/sys/fs/cgroup/cpuacct/cpu.cfs_period_us";
+        private const string CGROUP_V1_CPU_USAGE_FILEPATH = "/sys/fs/cgroup/cpuacct/cpuacct.usage";
 
         internal static readonly string[] RequiredFiles = new[]
         {
             MEMINFO_FILEPATH,
-            CPUSTAT_FILEPATH
+            CPUSTAT_FILEPATH,
+            CGROUP_V1_FILEPATH,
+            CGROUP_V1_USED_MEM_FILEPATH,
+            CGROUP_V1_TOTAL_MEM_FILEPATH,
+            CGROUP_V1_CFS_QUOTA_FILEPATH,
+            CGROUP_V1_CFS_PERIOD_FILEPATH,
+            CGROUP_V1_CPU_USAGE_FILEPATH
         };
 
         public LinuxEnvironmentStatistics(ILoggerFactory loggerFactory)
@@ -69,6 +84,13 @@ namespace Orleans.Statistics
         public async Task OnStart(CancellationToken ct)
         {
             _logger.LogTrace($"Starting {nameof(LinuxEnvironmentStatistics)}");
+
+            if (Directory.Exists(CGROUP_V1_FILEPATH))
+            {
+                _logger.LogTrace($"Process is running inside a CGroup, {nameof(LinuxEnvironmentStatistics)} " +
+                                 $"using the CGroup's limits instead of system ones");
+               InCGroup = true;
+            }
 
             _cts = new CancellationTokenSource();
             ct.Register(() => _cts.Cancel());
@@ -110,22 +132,43 @@ namespace Orleans.Statistics
 
         private async Task UpdateTotalPhysicalMemory()
         {
-            var memTotalLine = await ReadLineStartingWithAsync(MEMINFO_FILEPATH, "MemTotal");
-
-            if (string.IsNullOrWhiteSpace(memTotalLine))
+            if (InCGroup)
             {
-                _logger.LogWarning($"Couldn't read 'MemTotal' line from '{MEMINFO_FILEPATH}'");
-                return;
-            }
+                var memTotalLine = await ReadLineAsync(CGROUP_V1_TOTAL_MEM_FILEPATH);
 
-            // Format: "MemTotal:       16426476 kB"
-            if (!long.TryParse(new string(memTotalLine.Where(char.IsDigit).ToArray()), out var totalMemInKb))
+                if (string.IsNullOrWhiteSpace(memTotalLine))
+                {
+                    _logger.LogWarning($"Couldn't read line from '{CGROUP_V1_TOTAL_MEM_FILEPATH}'");
+                    return;
+                }
+
+                if (!long.TryParse(new string(memTotalLine.Where(char.IsDigit).ToArray()), out var totalMemInB))
+                {
+                    _logger.LogWarning($"Couldn't parse memory.limit_in_bytes output");
+                    return;
+                }
+
+                TotalPhysicalMemory = totalMemInB;
+            }
+            else
             {
-                _logger.LogWarning($"Couldn't parse meminfo output");
-                return;
-            }
+                var memTotalLine = await ReadLineStartingWithAsync(MEMINFO_FILEPATH, "MemTotal");
 
-            TotalPhysicalMemory = totalMemInKb * 1_000;
+                if (string.IsNullOrWhiteSpace(memTotalLine))
+                {
+                    _logger.LogWarning($"Couldn't read 'MemTotal' line from '{MEMINFO_FILEPATH}'");
+                    return;
+                }
+
+                // Format: "MemTotal:       16426476 kB"
+                if (!long.TryParse(new string(memTotalLine.Where(char.IsDigit).ToArray()), out var totalMemInKb))
+                {
+                    _logger.LogWarning($"Couldn't parse meminfo output");
+                    return;
+                }
+
+                TotalPhysicalMemory = totalMemInKb * 1_000;
+            }
         }
 
         private long _prevIdleTime;
@@ -133,64 +176,152 @@ namespace Orleans.Statistics
 
         private async Task UpdateCpuUsage(int i)
         {
-            var cpuUsageLine = await ReadLineStartingWithAsync(CPUSTAT_FILEPATH, "cpu  ");
-
-            if (string.IsNullOrWhiteSpace(cpuUsageLine))
+            if (InCGroup)
             {
-                _logger.LogWarning($"Couldn't read line from '{CPUSTAT_FILEPATH}'");
-                return;
+                var cpuUsageLine = await ReadLineAsync(CGROUP_V1_CPU_USAGE_FILEPATH);
+                var cpuUsageQuota = await ReadLineAsync(CGROUP_V1_CFS_QUOTA_FILEPATH);
+                var cpuUsagePeriod = await ReadLineAsync(CGROUP_V1_CFS_PERIOD_FILEPATH);
+
+                if (string.IsNullOrWhiteSpace(cpuUsageLine))
+                {
+                    _logger.LogWarning($"Couldn't read line from '{CGROUP_V1_CPU_USAGE_FILEPATH}'");
+                    return;
+                }
+
+                if (long.TryParse(cpuUsageLine, out _))
+                {
+                    _logger.LogWarning($"Failed to parse '{CGROUP_V1_CPU_USAGE_FILEPATH}' output correctly. Line: {cpuUsageLine}");
+                    return;
+                }
+
+                var cpuUsageNs = long.Parse(cpuUsageLine);
+
+                if (string.IsNullOrWhiteSpace(cpuUsageQuota))
+                {
+                    _logger.LogWarning($"Couldn't read line from '{CGROUP_V1_CFS_QUOTA_FILEPATH}'");
+                    return;
+                }
+
+                if (long.TryParse(cpuUsageQuota, out _))
+                {
+                    _logger.LogWarning($"Failed to parse '{CGROUP_V1_CFS_QUOTA_FILEPATH}' output correctly. Line: {cpuUsageLine}");
+                    return;
+                }
+
+                var cpuUsageQuotaUs = long.Parse(cpuUsageQuota);
+
+                if (string.IsNullOrWhiteSpace(cpuUsagePeriod))
+                {
+                    _logger.LogWarning($"Couldn't read line from '{CGROUP_V1_CFS_PERIOD_FILEPATH}'");
+                    return;
+                }
+
+                if (long.TryParse(cpuUsagePeriod, out _))
+                {
+                    _logger.LogWarning($"Failed to parse '{CGROUP_V1_CFS_PERIOD_FILEPATH}' output correctly. Line: {cpuUsageLine}");
+                    return;
+                }
+
+                var cpuUsagePeriodUs = long.Parse(cpuUsagePeriod);
+
+                if (i > 0)
+                {
+                    var deltaTotalTime = cpuUsageNs - _prevTotalTime;
+
+                    // 1 ns = 1000us
+                    // 1 s = 1000000us
+                    // Total cpu time quota: cfs_quota_us * (MONITOR_PERIOD / cfs_period_us)
+                    var currentCpuUsage = ((deltaTotalTime * 1_000) / (cpuUsageQuotaUs * ((float)MONITOR_PERIOD.Seconds * 1_000_000) / cpuUsagePeriodUs)) * 100f;
+
+                    var previousCpuUsage = CpuUsage ?? 0f;
+                    CpuUsage = (previousCpuUsage + 2 * currentCpuUsage) / 3;
+                }
+
+                _prevTotalTime = cpuUsageNs;
             }
-
-            // Format: "cpu  20546715 4367 11631326 215282964 96602 0 584080 0 0 0"
-            var cpuNumberStrings = cpuUsageLine.Split(' ').Skip(2);
-
-            if (cpuNumberStrings.Any(n => !long.TryParse(n, out _)))
+            else
             {
-                _logger.LogWarning($"Failed to parse '{CPUSTAT_FILEPATH}' output correctly. Line: {cpuUsageLine}");
-                return;
+                var cpuUsageLine = await ReadLineStartingWithAsync(CPUSTAT_FILEPATH, "cpu  ");
+
+                if (string.IsNullOrWhiteSpace(cpuUsageLine))
+                {
+                    _logger.LogWarning($"Couldn't read line from '{CPUSTAT_FILEPATH}'");
+                    return;
+                }
+
+                // Format: "cpu  20546715 4367 11631326 215282964 96602 0 584080 0 0 0"
+                var cpuNumberStrings = cpuUsageLine.Split(' ').Skip(2);
+
+                if (cpuNumberStrings.Any(n => !long.TryParse(n, out _)))
+                {
+                    _logger.LogWarning($"Failed to parse '{CPUSTAT_FILEPATH}' output correctly. Line: {cpuUsageLine}");
+                    return;
+                }
+
+                var cpuNumbers = cpuNumberStrings.Select(long.Parse).ToArray();
+                var idleTime = cpuNumbers[3];
+                var iowait = cpuNumbers[4]; // Iowait is not real cpu time
+                var totalTime = cpuNumbers.Sum() - iowait;
+
+                if (i > 0)
+                {
+                    var deltaIdleTime = idleTime - _prevIdleTime;
+                    var deltaTotalTime = totalTime - _prevTotalTime;
+
+                    var currentCpuUsage = (1.0f - deltaIdleTime / ((float)deltaTotalTime)) * 100f;
+
+                    var previousCpuUsage = CpuUsage ?? 0f;
+                    CpuUsage = (previousCpuUsage + 2 * currentCpuUsage) / 3;
+                }
+
+                _prevIdleTime = idleTime;
+                _prevTotalTime = totalTime;
+
             }
-
-            var cpuNumbers = cpuNumberStrings.Select(long.Parse).ToArray();
-            var idleTime = cpuNumbers[3];
-            var iowait = cpuNumbers[4]; // Iowait is not real cpu time
-            var totalTime = cpuNumbers.Sum() - iowait;
-
-            if (i > 0)
-            {
-                var deltaIdleTime = idleTime - _prevIdleTime;
-                var deltaTotalTime = totalTime - _prevTotalTime;
-
-                var currentCpuUsage = (1.0f - deltaIdleTime / ((float)deltaTotalTime)) * 100f;
-
-                var previousCpuUsage = CpuUsage ?? 0f;
-                CpuUsage = (previousCpuUsage + 2 * currentCpuUsage) / 3;
-            }
-
-            _prevIdleTime = idleTime;
-            _prevTotalTime = totalTime;
         }
 
         private async Task UpdateAvailableMemory()
         {
-            var memAvailableLine = await ReadLineStartingWithAsync(MEMINFO_FILEPATH, "MemAvailable");
-
-            if (string.IsNullOrWhiteSpace(memAvailableLine))
+            if (InCGroup)
             {
-                memAvailableLine = await ReadLineStartingWithAsync(MEMINFO_FILEPATH, "MemFree");
-                if (string.IsNullOrWhiteSpace(memAvailableLine))
+                var memTotalLine = await ReadLineAsync(CGROUP_V1_USED_MEM_FILEPATH);
+
+                if (string.IsNullOrWhiteSpace(memTotalLine))
                 {
-                    _logger.LogWarning($"Couldn't read 'MemAvailable' or 'MemFree' line from '{MEMINFO_FILEPATH}'");
+                    _logger.LogWarning($"Couldn't read line from '{CGROUP_V1_USED_MEM_FILEPATH}'");
                     return;
                 }
-            }
 
-            if (!long.TryParse(new string(memAvailableLine.Where(char.IsDigit).ToArray()), out var availableMemInKb))
+                if (!long.TryParse(new string(memTotalLine.Where(char.IsDigit).ToArray()), out var usedMemInB))
+                {
+                    _logger.LogWarning($"Couldn't parse memory.usage_in_bytes output");
+                    return;
+                }
+
+                AvailableMemory = usedMemInB;
+            }
+            else
             {
-                _logger.LogWarning($"Couldn't parse meminfo output: '{memAvailableLine}'");
-                return;
-            }
+                var memAvailableLine = await ReadLineStartingWithAsync(MEMINFO_FILEPATH, "MemAvailable");
 
-            AvailableMemory = availableMemInKb * 1_000;
+                if (string.IsNullOrWhiteSpace(memAvailableLine))
+                {
+                    memAvailableLine = await ReadLineStartingWithAsync(MEMINFO_FILEPATH, "MemFree");
+                    if (string.IsNullOrWhiteSpace(memAvailableLine))
+                    {
+                        _logger.LogWarning($"Couldn't read 'MemAvailable' or 'MemFree' line from '{MEMINFO_FILEPATH}'");
+                        return;
+                    }
+                }
+
+                if (!long.TryParse(new string(memAvailableLine.Where(char.IsDigit).ToArray()), out var availableMemInKb))
+                {
+                    _logger.LogWarning($"Couldn't parse meminfo output: '{memAvailableLine}'");
+                    return;
+                }
+
+                AvailableMemory = availableMemInKb * 1_000;
+            }
         }
 
         private void WriteToStatistics()
@@ -254,6 +385,21 @@ namespace Orleans.Statistics
                 if (i < 2)
                     i++;
             }
+        }
+
+        private static async Task<string> ReadLineAsync(string path)
+        {
+            using (var fs = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.Read, 512, FileOptions.SequentialScan | FileOptions.Asynchronous))
+            using (var r = new StreamReader(fs, Encoding.ASCII))
+            {
+                var line = await r.ReadLineAsync();
+                if (line is not null)
+                {
+                    return await r.ReadLineAsync();
+                }
+            }
+
+            return null;
         }
 
         private static async Task<string> ReadLineStartingWithAsync(string path, string lineStartsWith)
